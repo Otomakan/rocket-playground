e6f940f7667694b4706f65962eaf47e9eccbc9d2
Otomakan <jgmisteli@gmail.com>
2018-11-10 13:47:29 +0900

    Figuring out the Rocket Routing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+extern crate rocket_contrib;
+use rocket_contrib::serve::StaticFiles;
+// use Rock
+
+use rocket::Request;
+
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
+
+// fn main() {
+//     rocket::ignite().mount("/", StaticFiles::from("static")).launch();
+// }
+// #[get("/")]
+// fn main() {
+//     rocket::ignite()
+//         .mount("/public", StaticFiles::from("../static_files"))
+//         .launch();
+// }
+// #[get("/myfile")]
+fn myfiles()-> StaticFiles{
+	StaticFiles::from("static_files")
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/lol", 
+	StaticFiles::from("static_files"))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
